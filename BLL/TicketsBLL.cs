b026a1e1189dc9d91241869c35ec2ae7f5e121f9
YokairaTaveras <143297254+YokairaTaveras@users.noreply.github.com>
2023-10-03 18:12:41 -0400

    Creando TicketsBLL

---
+++ BLL/TicketsBLL.cs
@@ -0,0 +1,59 @@
+﻿using Microsoft.EntityFrameworkCore;
+using RegistroTickets.DAL;
+using RegistroTickets.Models;
+using System.Linq.Expressions;
+
+namespace RegistroTickets.BLL
+{
+    public class TicketsBLL
+    {
+        private readonly TicketsContext _context;
+
+        public TicketsBLL(TicketsContext context)
+        {
+            _context = context;
+        }
+        public bool Existe(int TicketId)
+        {
+            return _context.Sistemas.Any(S => S.TicketId == TicketId);
+        }
+        public bool Insertar(Sistemas Ticket)
+        {
+            _context.Sistemas.Add(Ticket);
+            int cantidad = _context.SaveChanges();
+            return cantidad > 0;
+        }
+        public bool Modificar(Sistemas Ticket)
+        {
+            _context.Update(Ticket);
+            int cantidad = _context.SaveChanges();
+            return cantidad > 0;
+        }
+        public bool Guardar(Sistemas Ticket)
+        {
+            if (!Existe(Ticket.TicketId))
+                return Insertar(Ticket);
+            else
+                return Modificar(Ticket);
+        }
+        public bool Eliminar(Sistemas Ticket)
+        {
+            _context.Sistemas.Remove(Ticket);
+            int cantidad = _context.SaveChanges();
+            return cantidad > 0;
+        }
+        public Sistemas? Buscar(int SistemaId)
+        {
+            return _context.Sistemas
+                .AsNoTracking()
+                .FirstOrDefault(s => s.SistemaId == SistemaId);
+        }
+        public List<Sistemas> GetList(Expression<Func<Sistemas, bool>> Criterio)
+        {
+            return _context.Sistemas
+                .Where(Criterio)
+                .AsNoTracking()
+                .ToList();
+        }
+    }
+}
